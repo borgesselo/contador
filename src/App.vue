1af8b8946f2e8bef8c5ea3d2991eda4806f00fa5
borgesselo <eloisaborgesif@gmail.com>
2025-03-28 09:01:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const contador = ref(0)
+
+function incrementar() {
+  contador.value++
+  if (contador.value == 10) {
+    alert(`aa`)
+  }
+}
+
+function decrementar() {
+  contador.value--
+}
+</script>
+
+<template>
+  <h1>Contador</h1>
+  <p>Contador: {{ contador }}</p>
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+</template>
+
+<style scoped>
+</style>
