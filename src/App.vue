f0120d0ba4b24a78443ac16d9643b5e79e414076
borgesselo <eloisaborgesif@gmail.com>
2025-03-28 09:21:31 -0300

    botaoreiniciar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <script setup>
-import { ref } from "vue";
+  import { ref } from 'vue';
 
-const contador = ref(0)
+  const contador = ref(0);
 
-function incrementar() {
-  contador.value++
-  if (contador.value == 10) {
-    alert(`aa`)
+  function incrementar() {
+    contador.value++;
   }
-}
 
-function decrementar() {
-  contador.value--
-}
+  function decrementar() {
+    if (contador.value > 0) {
+      contador.value--;
+    }
+  }
+
+  function reiniciar() {
+    contador.value = 0;
+  }
 </script>
 
 <template>
-  <h1>Contador</h1>
-  <p>Contador: {{ contador }}</p>
-  <button @click="incrementar">+</button>
-  <button @click="decrementar">-</button>
+  <div>
+    <h1>Contador</h1>
+    <p> {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+  </div>
 </template>
-
-<style scoped>
-</style>
